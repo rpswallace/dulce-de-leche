74ac3f4743e2ce83a03763de6bbd9680e92bb5cb
Randy Pazos <randyp@thehangar.cr>
2022-08-16 15:14:31 -0600

    Heroku configs

---
+++ back-end/config/env/production/database.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cbb36198d5372d2bcddda21c21d0a003'),
+    },
+  },
+});
